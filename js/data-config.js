3f8776d173706cf342c6d11f81f374b710e11d0c
topomat12138 <pulse3187@gmail.com>
2026-04-11 20:25:11 -0400

    modified:   .github/workflows/run.yml

--- js/data-config.js
+++ js/data-config.js
@@ -12,7 +12,7 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'dw-dengwei',
+    repoOwner: 'topomat12138',
 
     /**
      * GitHub repository name
@@ -43,4 +43,3 @@ const DATA_CONFIG = {
         return `${this.getDataBaseUrl()}/${filePath}`;
     }
 };
-
